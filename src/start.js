743a491476d9c2f295c69b8a3bbca76b8b31769a
Han Li <hanli1996@gmail.com>
2020-05-15 00:48:34 -0700

    setup react and live reload

--- src/start.js
+++ src/start.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+// require('electron-reload')
 
 function createWindow () {
   // Create the browser window.
@@ -10,11 +12,13 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  )
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  win.removeMenu()
 }
 
 // This method will be called when Electron has finished
